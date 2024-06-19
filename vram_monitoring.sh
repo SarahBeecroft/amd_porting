3c6a0006180543e6a722c99543ffee32c2743106
SarahBeecroft <Sarah.beecroft@pawsey.org.au>
2024-06-19 22:23:18 +0800

    Update vram_monitoring.sh

--- vram_monitoring.sh
+++ vram_monitoring.sh
@@ -7,7 +7,8 @@ while true; do
     # Check if the SLURM job is still running
     if squeue -j $SLURM_JOB_ID --noheader --format="%t" | grep -q "R\|PD"; then
         # Log the date and VRAM usage if the job is still running
-        echo echo -e "$(date +"%Y-%m-%d %H:%M:%S")\n$(rocm-smi --showmemuse | grep 'use' | awk '{print $7}')" >> ${SLURM_JOB_ID}_vram_usage.log
+        #echo -e "$(date +"%Y-%m-%d %H:%M:%S")\n$(rocm-smi --showmemuse | grep 'use' | awk '{print $7}')" >> ${SLURM_JOB_ID}_vram_usage.log
+        echo "$(date +"%Y-%m-%d %H:%M:%S") $(rocm-smi | awk '{print $9}') $(rocm-smi | awk '{print $10}')" >> ${SLURM_JOB_ID}_vram_usage.log"
         sleep 60
     else
         # Exit the loop if the job is no longer running
@@ -15,3 +16,10 @@ while true; do
         break
     fi
 done
+
+#
+#while true; do
+#    echo -e "$(date +"%Y-%m-%d %H:%M:%S")\n$(rocm-smi --showmemuse | grep 'use' | awk '{print $7}')" >> vram_usage.log
+#    sleep 60
+#done
+#
